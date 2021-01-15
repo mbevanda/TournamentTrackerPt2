183545352c561ef0ac936f872e102c329c262c68
Maja Bevanda <maja.bevanda@hotmail.com>
2021-01-15 16:39:42 +0100

    Initializatied db connection

--- TournamentTracker/MVCUI/Global.asax.cs
+++ TournamentTracker/MVCUI/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TrackerLibrary;
 
 namespace MVCUI
 {
@@ -16,6 +17,9 @@ namespace MVCUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the database connections
+            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.Sql);
         }
     }
 }
