2f68129c6b27518fa556373d05a8cba54581e49a
Maja Bevanda <maja.bevanda@hotmail.com>
2021-01-15 16:40:05 +0100

    Populated tourney list on homepage

--- TournamentTracker/MVCUI/Controllers/HomeController.cs
+++ TournamentTracker/MVCUI/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrackerLibrary;
+using TrackerLibrary.Models;
 
 namespace MVCUI.Controllers
 {
@@ -10,21 +12,9 @@ namespace MVCUI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
+            List<TournamentModel> tournaments = GlobalConfig.Connection.GetTournament_All();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(tournaments);
         }
     }
 }
